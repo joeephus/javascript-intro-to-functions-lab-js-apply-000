74b10a3d32fef0dd7ceb3397b00d00aeaca3950d
Joseph Lorenzo <josephclorenzo@gmail.com>
2018-10-11 22:54:21 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,9 +1,7 @@
 
-describe('shout(string)', function() {
-  it('receives one argument and returns it in all caps', function() {
-    expect(shout('hello')).toEqual('HELLO')
-  })
-})
+function shout(string) {
+  return string.toUpperCase()
+}
 
 describe('whisper(string)', function() {
   it('receives one argument and returns it in all lowercase', function() {
