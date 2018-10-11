6f84addc0d1100a2c2fe7af9e932b5f5c8d6b36e
Joseph Lorenzo <josephclorenzo@gmail.com>
2018-10-11 23:03:11 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -4,7 +4,7 @@ function whisper(string) {
 }
 
 function logShout(string) {
-   console.log(string.toUpperCase());
+  console.log(string.toUpperCase());
 }
 
 describe('logShout(string)', function() {
