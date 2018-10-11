8b8d7cefc86226f953e14ac5bc12a6be6b32ce66
Joseph Lorenzo <josephclorenzo@gmail.com>
2018-10-11 23:04:30 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -4,20 +4,10 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log(string.toUpperCase());
+  return console.log(string.toUpperCase());
 }
 
-describe('logShout(string)', function() {
-  it('calls console.log() its one argument in all caps', function() {
-    const spy = expect.spyOn(console, 'log').andCallThrough()
 
-    logShout('hello')
-
-    expect(spy).toHaveBeenCalledWith('HELLO')
-
-    console.log.restore()
-  })
-})
 
 describe('logWhisper(string)', function() {
   it('calls console.log() its one argument in all lowercase', function() {
